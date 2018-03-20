1d91624cb0dc74d89e590a70226210c4197e5b3f
Derbyshire <36576655+Derbyshire@users.noreply.github.com>
2018-03-20 11:12:18 +0000

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for a juggling licence',
+  serviceName: 'Pension',
 
   // Default port that prototype runs on
   port: '3000',
